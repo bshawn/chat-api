be71ab70b262d71f8af7b5fef4e2a19cfefac8dc
bshawn <bret.shawn@gmail.com>
2017-10-27 21:42:47 -0400

    Finish adding CRUD logic for user

--- src/Controllers/UsersController.cs
+++ src/Controllers/UsersController.cs
@@ -16,37 +16,61 @@ namespace ChatApi.Controllers
 
         // GET api/users
         [HttpGet]
-        public IEnumerable<User> Get()
+        public IActionResult Get()
         {
-            var client = new MongoClient(connString);
-            var db = client.GetDatabase("chat-demo");
-            var collection = db.GetCollection<User>("users");
-            return collection.Find(new BsonDocument()).ToList();
+            var collection = GetUserCollection();
+            var users = collection.Find(u => true).ToList();
+            return Json(users);
         }
 
-        // GET api/users/5
+        // GET api/users/59f3d3c225eca06c14be4694
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(string id)
         {
-            return "value";
+            var collection = GetUserCollection();
+            var user = collection.Find(u => u.Id == id).FirstOrDefault();
+
+            if (user == null)
+                return NotFound();
+
+            return Json(user);
         }
 
         // POST api/users
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]User user)
         {
+            var collection = GetUserCollection();
+            user.Id = ObjectId.GenerateNewId().ToString();
+            collection.InsertOne(user);
+            return Get(user.Id);
         }
 
-        // PUT api/users/5
+        // PUT api/users/59f3d3c225eca06c14be4694
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(string id, [FromBody]User user)
         {
+            var collection = GetUserCollection();
+            user.Id = id;
+            collection.ReplaceOne(u => u.Id == id, user);
+            return Get(user.Id);
         }
 
-        // DELETE api/users/5
+        // DELETE api/users/59f3d3c225eca06c14be4694
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(string id)
         {
+            var collection = GetUserCollection();
+            collection.DeleteOne(u => u.Id == id);
+            return Ok();
+        }
+
+        private IMongoCollection<User> GetUserCollection()
+        {
+            var client = new MongoClient(connString);
+            var db = client.GetDatabase("chat-demo");
+            var collection = db.GetCollection<User>("users");
+            return collection;
         }
     }
 }
